8fdaae58a7e063529afe3761da9458f4d87af5c6
BigC1109 <66857091+BigC1109@users.noreply.github.com>
2023-04-01 19:31:35 -0400

    Finished the MetalBar

--- DevcadeGame/Game1.cs
+++ DevcadeGame/Game1.cs
@@ -1,38 +1,37 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
+﻿using Devcade;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
-using Devcade;
-using System.Diagnostics;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace DevcadeGame
 {
-	public class Game1 : Game
-	{
-		private GraphicsDeviceManager _graphics;
-		private SpriteBatch _spriteBatch;
-		private Texture2D metalBar;
-		private Tuple<int, int> Coordinates; // width, height
-		private List<int> Heights = new List<int> { 0, 0 };
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D metalBar;
+        private Tuple<int, int> Coordinates; // width, height
+        private Dictionary<int, int[]> metalBarPosition = new Dictionary<int, int[]>();
 
-		/// <summary>
-		/// Game constructor
-		/// </summary>
-		public Game1()
-		{
-			_graphics = new GraphicsDeviceManager(this);
-			Content.RootDirectory = "Content";
-			IsMouseVisible = false;
-		}
+        /// <summary>
+        /// Game constructor
+        /// </summary>
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = false;
+        }
 
-		/// <summary>
-		/// Does any setup prior to the first frame that doesn't need loaded content.
-		/// </summary>
-		protected override void Initialize()
-		{
-			Input.Initialize(); // Sets up the input library
+        /// <summary>
+        /// Does any setup prior to the first frame that doesn't need loaded content.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Input.Initialize(); // Sets up the input library
 
             // Set window size if running debug (in release it will be fullscreen)
             #region
@@ -46,7 +45,7 @@ namespace DevcadeGame
             //_graphics.ApplyChanges();
 
             // Connor's massive monitor size requires this
-            Coordinates = Tuple.Create(750, 1750);
+            // Coordinates = Tuple.Create(750, 1750);
             _graphics.PreferredBackBufferWidth = Coordinates.Item1;
             _graphics.PreferredBackBufferHeight = Coordinates.Item2;
             _graphics.ApplyChanges();
@@ -57,99 +56,119 @@ namespace DevcadeGame
 #endif
             #endregion
 
+            metalBarPosition[0] = new int[] { 0, Coordinates.Item2 - 30 };
+            metalBarPosition[1] = new int[] { Coordinates.Item1, Coordinates.Item2 - 30};
+
             // TODO: Add your initialization logic here
 
             base.Initialize();
-		}
+        }
 
-		/// <summary>
-		/// Does any setup prior to the first frame that needs loaded content.
-		/// </summary>
-		protected override void LoadContent()
-		{
-			_spriteBatch = new SpriteBatch(GraphicsDevice);
+        /// <summary>
+        /// Does any setup prior to the first frame that needs loaded content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			metalBar = Content.Load<Texture2D>("download");
+            metalBar = Content.Load<Texture2D>("download");
 
 
 
-			// TODO: use this.Content to load your game content here
-			// ex.
-			// texture = Content.Load<Texture2D>("fileNameWithoutExtention");
-		}
+            // TODO: use this.Content to load your game content here
+            // ex.
+            // texture = Content.Load<Texture2D>("fileNameWithoutExtention");
+        }
 
-		/// <summary>
-		/// Your main update loop. This runs once every frame, over and over.
-		/// </summary>
-		/// <param name="gameTime">This is the gameTime object you can use to get the time since last frame.</param>
-		protected override void Update(GameTime gameTime)
-		{
-			Input.Update(); // Updates the state of the input library
+        /// <summary>
+        /// Your main update loop. This runs once every frame, over and over.
+        /// </summary>
+        /// <param name="gameTime">This is the gameTime object you can use to get the time since last frame.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            Input.Update(); // Updates the state of the input library
 
-			// Exit when both menu buttons are pressed (or escape for keyboard debuging)
-			// You can change this but it is suggested to keep the keybind of both menu
-			// buttons at once for gracefull exit.
-			if (Keyboard.GetState().IsKeyDown(Keys.Escape) ||
-				(Input.GetButton(1, Input.ArcadeButtons.Menu) &&
-				Input.GetButton(2, Input.ArcadeButtons.Menu)))
-			{
-				Exit();
-			}
-
-			// TODO: Add your update logic here
-			if (Keyboard.GetState().IsKeyDown(Keys.Down))
-			{
-				Debug.WriteLine("Down (Left)");
-                var height = Heights[0];
-                Heights.RemoveAt(0);
-                height += 10;
-                Heights.Insert(0, height);
-            }
-			if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            // Exit when both menu buttons are pressed (or escape for keyboard debuging)
+            // You can change this but it is suggested to keep the keybind of both menu
+            // buttons at once for gracefull exit.
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape) ||
+                (Input.GetButton(1, Input.ArcadeButtons.Menu) &&
+                Input.GetButton(2, Input.ArcadeButtons.Menu)))
             {
+                Exit();
+            }
+
+            // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {                
                 Debug.WriteLine("Up (Left)");
-				var height = Heights[0];
-				Heights.RemoveAt(0);
-				height -= 10;
-				Heights.Insert(0, height);
+                metalBarPosition[0] = new int[] { 0, metalBarPosition[0][1] + 10 };
+                if (metalBarPosition[0][1] + 30 > Coordinates.Item2)
+                {
+                    metalBarPosition[0] = new int[] { 0, Coordinates.Item2 - 30};
+                }
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                Debug.WriteLine("Down (Left)");
+                metalBarPosition[0] = new int[] { 0, metalBarPosition[0][1] - 10 };
+                if (metalBarPosition[0][1] < 0)
+                {
+                    metalBarPosition[0] = new int[] { 0, 0 };
+                }
+
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Left))
             {
-                Debug.WriteLine("Down (Right)");
-                var height = Heights[1];
-                Heights.RemoveAt(1);
-                height += 10;
-                Heights.Insert(1, height);
+                Debug.WriteLine("Up (Right)");
+                metalBarPosition[1] = new int[] { Coordinates.Item1, metalBarPosition[1][1] + 10 };
+                if (metalBarPosition[1][1] + 30 > Coordinates.Item2)
+                {
+                    metalBarPosition[1] = new int[] { Coordinates.Item1, Coordinates.Item2 - 30};
+                }
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Right))
             {
-                Debug.WriteLine("Up (Right)");
-                var height = Heights[1];
-                Heights.RemoveAt(1);
-                height -= 10;
-                Heights.Insert(1, height);
+                Debug.WriteLine("Down (Right)");
+                metalBarPosition[1] = new int[] { Coordinates.Item1, metalBarPosition[1][1] - 10 };
+                if (metalBarPosition[1][1] < 0)
+                {
+                    metalBarPosition[1] = new int[] { Coordinates.Item1, 0 };
+                }
+
             }
 
-			Debug.WriteLine($"H1: {Heights[0]} | H2: {Heights[1]}");
+            // Debug.WriteLine($"H1: {Heights[0]} | H2: {Heights[1]}");
 
             base.Update(gameTime);
-		}
+        }
 
-		/// <summary>
-		/// Your main draw loop. This runs once every frame, over and over.
-		/// </summary>
-		/// <param name="gameTime">This is the gameTime object you can use to get the time since last frame.</param>
-		protected override void Draw(GameTime gameTime)
-		{
-			GraphicsDevice.Clear(new Color(227, 208, 36));
+        /// <summary>
+        /// Your main draw loop. This runs once every frame, over and over.
+        /// </summary>
+        /// <param name="gameTime">This is the gameTime object you can use to get the time since last frame.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(227, 208, 36));
 
             _spriteBatch.Begin();
-			// TODO: Add your drawing code here
-			//Debug.WriteLine($"x, y:{Coordinates} x:{Coordinates.Item1 / 2} y:{Coordinates.Item2 * (5 / 100)}");
-			_spriteBatch.Draw(metalBar, new Rectangle(-15, (Coordinates.Item2/2) + Heights[1], Coordinates.Item1 + 30, Coordinates.Item2/65), new Color(255, 255, 255));
-			_spriteBatch.End();
+            // TODO: Add your drawing code here
+            Debug.WriteLine("RightBar: " + metalBarPosition[1][0] + " " + metalBarPosition[1][1]);
+            Debug.WriteLine("LeftBar: " + metalBarPosition[0][0] + " " + metalBarPosition[0][1]);
+            int x1 = metalBarPosition[0][0];
+            int y1 = metalBarPosition[0][1];
+            int x2 = metalBarPosition[1][0];
+            int y2 = metalBarPosition[1][1];
+            int y = y2 - y1;
+            int x = x2 - x1;
+            Debug.WriteLine(x + " " + y);
+            double rotation = Math.Atan2(y, x);
+            Debug.WriteLine((float)rotation);
+            int[] midpoint = new int[] {(x1 + x2) / 2, (y1 + y2) / 2};
+            _spriteBatch.Draw(metalBar, new Rectangle(0, metalBarPosition[0][1], Coordinates.Item1 + 800, 30), null, new Color(255, 255, 255), (float)rotation, new Vector2(0, 0), SpriteEffects.None, 0);
+            _spriteBatch.End();
 
-			base.Draw(gameTime);
-		}
-	}
+            base.Draw(gameTime);
+        }
+    }
 }
